1eac97a4c4296c2959e6217a8c1214b4037b25e9
Sabhanya Golakoti <golakotisabhanya@gmail.com>
2026-06-10 06:06:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (21.20%)

--- 1848-sum-of-unique-elements/solution.py
+++ 1848-sum-of-unique-elements/solution.py
@@ -1,14 +1,10 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         ans=0
-        d=dict()
-        for num in nums:
-            if num in d:
-                d[num]+=1
-            else:
-                d[num]=1
-        for k,v in d.items():
-            if v==1:
-                ans+=k
+        count=Counter(nums)
+        for (key,val) in count.items():
+
+            if val==1:
+                ans+=key
         return ans
         
